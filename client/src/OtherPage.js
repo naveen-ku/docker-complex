85b6a0126ee742007a9d0087860c33b2f8119eab
naveen-ku <naveen2951996@gmail.com>
2022-02-11 11:37:09 +0530

    [fix and feat] adds docker-compose configuration and fixes code

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const OtherPage =  () => {
-	return (
-		<div>
-			I'm some other page!
-			<Link to="/"> Go back home </Link>
-		</div>
-		);
-} 
+const OtherPage = () => {
+  return (
+    <div>
+      Im some other page
+      <Link to="/">Go back to home page !!!</Link>
+    </div>
+  );
+};
 
 export default OtherPage;
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const keys = require('./keys');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const {Pool} = require('pg');
+const { Client } = require('pg');
 const redis = require('redis');
 
 
@@ -12,18 +12,23 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Postgress
-const pgClient = new Pool({
-	user: keys.pgUser,
+const pgClient = new Client ({
 	host: keys.pgHost,
+	user: keys.pgUser,
 	database: keys.pgDatabase,
 	password: keys.pgPassword,
-	port: keys.pgPort,
+	port: keys.pgPort
 });
-pgClient.on('error', () => console.log('Lost PG connection'));
+
+// Connect to postgress
+pgClient.connect()
+  .then(() => console.log('Postgress connected'))
+  .catch(err => console.error('Postgress connection error', err.stack))
 
 // Postgress Create table
 pgClient
 	.query('CREATE TABLE IF NOT EXISTS values (number INT)')
+	.then(() =>console.log("Table created"))
 	.catch(err => console.log(err));
 
 // Redis Client setup
@@ -44,10 +49,11 @@ app.get('/',(req,res) => {
 
 app.get('/values/all', async (req,res) => {
 	const values = await pgClient.query('SELECT * FROM values');
+	console.log(values);
 	res.send(values.rows);
 });
 
-app.get('values/current', async (req,res) => {
+app.get('/values/current', async (req,res) => {
 	redisClient.hgetall('values',(err, values) => {
 		res.send(values);
 	})
@@ -67,5 +73,5 @@ app.post('/values', async (req,res) => {
 });
 
 app.listen(5000,err=>{
-	console.log('Listening');
+	console.log('Server Listening at port 5000');
 });
